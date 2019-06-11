204128bf9de54f55c6328881cc490ed29df48d61
pieths.dev@gmail.com <PH Schouten>
2019-06-11 08:45:32 -0700

    Use float32 for data dtype in IidSpikeDetector example.

--- src/python/nimbusml/examples/examples_from_dataframe/IidSpikeDetector_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/IidSpikeDetector_df.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 from nimbusml.time_series import IidSpikeDetector
 
-X_train = pd.Series([5,5,5,5,5,10,5,5,5,5,5])
+X_train = pd.Series([5, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5], dtype=np.float32)
 
 isd = IidSpikeDetector()
-result = isd.fit_transform(X_train)
+result = isd.fit_transform(X_train, verbose=1)
 
 print(result)
