e92ffe1a9b30b8e0b8564a2d41106435023260a0
pieths.dev@gmail.com <PH Schouten>
2019-06-12 16:00:48 -0700

    Fix issues with the test_estimator_checks IidSpikeDetector tests.

--- src/python/tests/test_estimator_checks.py
+++ src/python/tests/test_estimator_checks.py
@@ -16,6 +16,7 @@ from nimbusml.feature_extraction.text import NGramFeaturizer
 from nimbusml.internal.entrypoints._ngramextractor_ngram import n_gram
 from nimbusml.preprocessing import TensorFlowScorer
 from nimbusml.preprocessing.filter import SkipFilter, TakeFilter
+from nimbusml.time_series import IidSpikeDetector
 from sklearn.utils.estimator_checks import _yield_all_checks, MULTI_OUTPUT
 
 this = os.path.abspath(os.path.dirname(__file__))
@@ -53,6 +54,8 @@ OMITTED_CHECKS = {
     # fix pending in PR, bug cant handle csr matrix
     'RangeFilter': 'check_estimators_dtypes, '
                    'check_estimator_sparse_data',
+    # time series does not currently support sparse matrices
+    'IidSpikeDetector': 'check_estimator_sparse_data',
     # bug, low tolerance
     'FastLinearRegressor': 'check_supervised_y_2d, '
                            'check_regressor_data_not_an_array, '
@@ -180,6 +183,7 @@ INSTANCES = {
     'NGramFeaturizer': NGramFeaturizer(word_feature_extractor=n_gram()),
     'SkipFilter': SkipFilter(count=5),
     'TakeFilter': TakeFilter(count=100000),
+    'IidSpikeDetector': IidSpikeDetector(columns=['F0']),
     'TensorFlowScorer': TensorFlowScorer(
         model_location=os.path.join(
             this,
