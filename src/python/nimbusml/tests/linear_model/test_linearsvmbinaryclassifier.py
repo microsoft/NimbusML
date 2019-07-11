0d2ebaccc2fc83ba1114437edbfc9c5641f46cfb
REDMOND\nakazmi <nakazmi@microsoft.com>
2019-07-10 17:39:47 -0700

    Add test for predict_proba() and decision_function()

--- src/python/nimbusml/tests/linear_model/test_linearsvmbinaryclassifier.py
+++ src/python/nimbusml/tests/linear_model/test_linearsvmbinaryclassifier.py
@@ -18,12 +18,12 @@ from nimbusml.linear_model import LinearSvmBinaryClassifier
 from nimbusml.datasets import get_dataset
 from nimbusml import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.utils.testing import assert_greater
+from sklearn.utils.testing import assert_almost_equal, assert_greater
 
 
 class TestLinearSvmBinaryClassifier(unittest.TestCase):
-
-    def test_linearsvm(self):
+    @classmethod
+    def setUpClass(self):
         np.random.seed(0)
         df = get_dataset("infert").as_df()
         # remove : and ' ' from column names, and encode categorical column
@@ -31,12 +31,32 @@ class TestLinearSvmBinaryClassifier(unittest.TestCase):
         assert is_string_dtype(df['education_str'].dtype)
         df = (OneHotVectorizer() << ['education_str']).fit_transform(df)
         assert 'education_str' not in df.columns
-        X_train, X_test, y_train, y_test = train_test_split(
-            df.loc[:, df.columns != 'case'], df['case'], random_state=0)
-        svm = LinearSvmBinaryClassifier(shuffle=False).fit(X_train, y_train)
-        scores = svm.predict(X_test)
-        accuracy = np.mean(y_test == [i for i in scores])
-        assert_greater(accuracy, 0.96, "accuracy should be %s" % 0.96)
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(df.loc[:, df.columns != 'case'],
+                             df['case'],
+                             random_state=0)
+        self.svm = LinearSvmBinaryClassifier(shuffle=False).fit(self.X_train,
+                                                                self.y_train)
+        self.predictions = self.svm.predict(self.X_test)
+        self.accuracy = np.mean(self.y_test == [i for i in self.predictions])
+
+    def test_linearsvm(self):
+        assert_greater(self.accuracy, 0.96, "accuracy should be %s" % 0.96)
+
+    def test_linearsvm_predict_proba(self):
+        probabilities = self.svm.predict_proba(self.X_test)
+        # Test that the class probabilities for each instance add up to 1
+        [assert_almost_equal(probabilities[i][0] + probabilities[i][1], 1) \
+            for i in range(probabilities.shape[0])]
+
+    def test_linearsvm_decision_function(self):
+        fn = self.svm.decision_function(self.X_test)
+        predictions_from_fn = [fn[i] >= 0 for i in range(len(fn))]
+        assert [predictions_from_fn[i] == self.predictions[i] \
+            for i in range(len(self.predictions))]
+        accuracy_from_fn = np.mean(
+            self.y_test == [i for i in predictions_from_fn])
+        assert accuracy_from_fn == self.accuracy
 
 
 if __name__ == '__main__':
