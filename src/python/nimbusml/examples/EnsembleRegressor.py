7aa02e64c246316f00b1cafb6e437d4c87d70b4e
REDMOND\nakazmi <nakazmi@microsoft.com>
2019-08-05 17:29:58 -0700

    Improve examples

--- src/python/nimbusml/examples/EnsembleRegressor.py
+++ src/python/nimbusml/examples/EnsembleRegressor.py
@@ -4,8 +4,10 @@ from nimbusml import Pipeline, FileDataStream
 from nimbusml.datasets import get_dataset
 from nimbusml.feature_extraction.categorical import OneHotVectorizer
 from nimbusml.ensemble import EnsembleRegressor
+from nimbusml.ensemble.feature_selector import RandomFeatureSelector
 from nimbusml.ensemble.output_combiner import RegressorMedian
-from nimbusml.ensemble.sub_model_selector import RegressorBestPerformanceSelector
+from nimbusml.ensemble.subset_selector import RandomPartitionSelector
+from nimbusml.ensemble.sub_model_selector import RegressorBestDiverseSelector
 
 # data input (as a FileDataStream)
 path = get_dataset('infert').as_filepath()
@@ -18,28 +20,59 @@ print(data.head())
 # 3   34     1    0-5yrs        2       4 ...       4            0  ...
 # 4   35     1   6-11yrs        1       3 ...       5            1  ...
 
-# define the training pipeline
-pipeline = Pipeline([
+# define the training pipeline using default sampling and ensembling parameters
+pipeline_with_defaults = Pipeline([
     OneHotVectorizer(columns={'edu': 'education'}),
-    EnsembleRegressor(feature=['induced', 'edu'], label='age', num_models=3,
-                      sub_model_selector_type=RegressorBestPerformanceSelector(),
-                      output_combiner=RegressorMedian())
+    EnsembleRegressor(feature=['induced', 'edu'], label='age', num_models=3)
 ])
 
 # train, predict, and evaluate
-metrics, predictions = pipeline.fit(data).test(data, output_scores=True)
+metrics, predictions = pipeline_with_defaults.fit(data).test(data, output_scores=True)
 
 # print predictions
 print(predictions.head())
 #        Score
-# 0  24.243475
-# 1  24.243475
-# 2  24.978357
-# 3  24.978357
-# 4  33.913101
+# 0  26.046741
+# 1  26.046741
+# 2  29.225840
+# 3  29.225840
+# 4  33.849384
 
 # print evaluation metrics
 print(metrics)
-#     L1(avg)    L2(avg)  RMS(avg)  Loss-fn(avg)  R Squared
-# 0  4.498702  31.549082  5.616857     31.549082  -0.148587
+#    L1(avg)    L2(avg)  RMS(avg)  Loss-fn(avg)  R Squared
+# 0  4.69884  33.346123   5.77461     33.346124  -0.214011
 
+
+# define the training pipeline with specific sampling and ensembling options
+pipeline_with_options = Pipeline([
+    OneHotVectorizer(columns={'edu': 'education'}),
+    EnsembleRegressor(feature=['induced', 'edu'],
+                      label='age',
+                      num_models=3,
+                      sampling_type = RandomPartitionSelector(
+                          feature_selector=RandomFeatureSelector(
+                               features_selction_proportion=0.7)),
+                       sub_model_selector_type=RegressorBestDiverseSelector(),
+                       output_combiner=RegressorMedian())
+])
+
+# train, predict, and evaluate
+metrics, predictions = pipeline_with_options.fit(data).test(data, output_scores=True)
+
+# print predictions
+print(predictions.head())
+#        Score
+# 0  37.122200
+# 1  37.122200
+# 2  41.296204
+# 3  41.296204
+# 4  33.591423
+
+# print evaluation metrics
+# note that the converged loss function values are worse than with defaults as
+# this is a small dataset that we partition into 3 chunks for each regressor,
+# which decreases model quality
+print(metrics)
+#     L1(avg)    L2(avg)  RMS(avg)  Loss-fn(avg)  R Squared
+# 0  5.481676  44.924838  6.702599     44.924838   -0.63555
