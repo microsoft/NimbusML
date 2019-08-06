391d859e0de17cfa526b68681b9e21ab12d65a84
REDMOND\nakazmi <nakazmi@microsoft.com>
2019-08-06 16:23:37 -0700

    Add --installPythonPackages flag to build scripts

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ usage()
     echo "Options:"
     echo "  --configuration <Configuration>   Build Configuration (DbgLinPy3.7,DbgLinPy3.6,DbgLinPy3.5,DbgLinPy2.7,RlsLinPy3.7,RlsLinPy3.6,RlsLinPy3.5,RlsLinPy2.7,DbgMacPy3.7,DbgMacPy3.6,DbgMacPy3.5,DbgMacPy2.7,RlsMacPy3.7,RlsMacPy3.6,RlsMacPy3.5,RlsMacPy2.7)"
     echo "  --runTests                        Run tests after build"
+    echo "  --installPythonPackages           Install python packages after build"
     echo "  --runTestsOnly                    Run tests on a wheel file in default build location (<repo>/target/)"
     echo "  --includeExtendedTests            Include the extended tests if the tests are run"
     echo "  --buildNativeBridgeOnly           Build only the native bridge code"
@@ -31,6 +32,7 @@ else
     __configuration=DbgLinPy3.7
 fi
 __runTests=false
+__installPythonPackages=false
 __runExtendedTests=false
 __buildNativeBridge=true
 __buildDotNetBridge=true
@@ -48,6 +50,10 @@ while [ "$1" != "" ]; do
             ;;
         --runtests)
             __runTests=true
+            __installPythonPackages=true
+            ;;
+        --installPythonPackages)
+            __installPythonPackages=true
             ;;
         --includeextendedtests)
             __runExtendedTests=true
@@ -247,11 +253,11 @@ then
     echo Python package successfully created: ${__currentScriptDir}/target/${WheelFile}
 fi
 
-if [ ${__runTests} = true ]
-then 
+if [ ${__installPythonPackages} = true ]
+then
     echo ""
     echo "#################################"
-    echo "Running tests ... "
+    echo "Installing Python packages ... "
     echo "#################################"
     Wheel=${__currentScriptDir}/target/nimbusml-${ProductVersion}-${PythonTag}-none-${PlatName}.whl
     if [ ! -f ${Wheel} ]
@@ -274,6 +280,12 @@ then
     "${PythonExe}" -m pip install --upgrade "${Wheel}"
     "${PythonExe}" -m pip install "scikit-learn==0.19.2"
 
+if [ ${__runTests} = true ]
+then 
+    echo ""
+    echo "#################################"
+    echo "Running tests ... "
+    echo "#################################"
     PackagePath=${PythonRoot}/lib/python${PythonVersion}/site-packages/nimbusml
     TestsPath1=${PackagePath}/tests
     TestsPath2=${__currentScriptDir}/src/python/tests
