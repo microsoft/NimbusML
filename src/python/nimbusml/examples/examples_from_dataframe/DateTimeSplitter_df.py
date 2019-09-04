96904c429996ad54f2c33cda69f126581e7fafa2
pieths.dev@gmail.com <PH Schouten>
2019-09-04 14:58:13 -0700

    Update the examples and unit tests.

--- src/python/nimbusml/examples/examples_from_dataframe/DateTimeSplitter_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/DateTimeSplitter_df.py
@@ -4,8 +4,8 @@ import pandas
 from nimbusml.preprocessing import DateTimeSplitter
 
 df = pandas.DataFrame(data=dict(
-    tokens1=['one_' + str(i) for i in range(8)],
-    tokens2=['two_' + str(i) for i in range(8)]
+    tokens1=[1, 2, 3, 4],
+    tokens2=[10, 11, 12, 13]
 ))
 
 # duplicate a column
@@ -13,4 +13,5 @@ cd = DateTimeSplitter(prefix='dt') << {'tokens3': 'tokens1'}
 y = cd.fit_transform(df)
 
 # view the three columns
+pandas.set_option('display.max_columns', None)
 print(y)
