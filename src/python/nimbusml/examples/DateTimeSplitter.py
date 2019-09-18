b1a8073fb2e7ee74993321bfbed176bc77832b92
pieths.dev@gmail.com <PH Schouten>
2019-09-18 15:32:00 -0700

    Update the DateTimeSplitter examples.

--- src/python/nimbusml/examples/DateTimeSplitter.py
+++ src/python/nimbusml/examples/DateTimeSplitter.py
@@ -1,5 +1,6 @@
 ###############################################################################
 # DateTimeSplitter
+import pandas as pd
 from nimbusml import FileDataStream
 from nimbusml.datasets import get_dataset
 from nimbusml.preprocessing import DateTimeSplitter
@@ -15,11 +16,17 @@ xf = DateTimeSplitter(prefix='dt_') << 'age'
 # fit and transform
 features = xf.fit_transform(data)
 
+features = features.drop(['row_num', 'education', 'parity', 'induced',
+                          'case', 'spontaneous', 'stratum', 'pooled.stratum'], axis=1)
+
 # print features
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
 print(features.head())
-#   age  age_copy  case education education_copy  induced  parity  ...
-# 0   26        26     1    0-5yrs         0-5yrs        1       6  ...
-# 1   42        42     1    0-5yrs         0-5yrs        1       1  ...
-# 2   39        39     1    0-5yrs         0-5yrs        2       6  ...
-# 3   34        34     1    0-5yrs         0-5yrs        2       4  ...
-# 4   35        35     1   6-11yrs        6-11yrs        1       3  ...
+
+#    age  dt_Year  dt_Month  dt_Day  dt_Hour  dt_Minute  dt_Second  dt_AmPm  dt_Hour12  dt_DayOfWeek  dt_DayOfQuarter  dt_DayOfYear  dt_WeekOfMonth  dt_QuarterOfYear  dt_HalfOfYear  dt_WeekIso  dt_YearIso dt_MonthLabel dt_AmPmLabel dt_DayOfWeekLabel   dt_HolidayName  dt_IsPaidTimeOff
+# 0   26     1970         1       1        0          0         37        0          0             4                1             0               0                 1              1           1        1970       January           am          Thursday  NOT IMPLEMENTED                 0
+# 1   42     1970         1       1        0          0         42        0          0             4                1             0               0                 1              1           1        1970       January           am          Thursday  NOT IMPLEMENTED                 0
+# 2   39     1970         1       1        0          0         39        0          0             4                1             0               0                 1              1           1        1970       January           am          Thursday  NOT IMPLEMENTED                 0
+# 3   34     1970         1       1        0          0         34        0          0             4                1             0               0                 1              1           1        1970       January           am          Thursday  NOT IMPLEMENTED                 0
+# 4   35     1970         1       1        0          0         38        0          0             4                1             0               0                 1              1           1        1970       January           am          Thursday  NOT IMPLEMENTED                 0
