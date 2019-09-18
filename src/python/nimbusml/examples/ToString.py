d434492d676d374fc4216ad70b7f1631a8ccf15f
pieths.dev@gmail.com <PH Schouten>
2019-09-18 16:30:27 -0700

    Update the ToString examples.

--- src/python/nimbusml/examples/ToString.py
+++ src/python/nimbusml/examples/ToString.py
@@ -11,12 +11,12 @@ path = get_dataset('infert').as_filepath()
 data = FileDataStream.read_csv(path, sep=',', numeric_dtype=numpy.float32,
                                names={0: 'id'})
 print(data.head())
-#    age  case education   id  induced  parity  pooled.stratum  spontaneous ...
-# 0  26.0   1.0    0-5yrs  1.0      1.0     6.0             3.0         2.0 ...
-# 1  42.0   1.0    0-5yrs  2.0      1.0     1.0             1.0         0.0 ...
-# 2  39.0   1.0    0-5yrs  3.0      2.0     6.0             4.0         0.0 ...
-# 3  34.0   1.0    0-5yrs  4.0      2.0     4.0             2.0         0.0  ..
-# 4  35.0   1.0   6-11yrs  5.0      1.0     3.0            32.0         1.0  ..
+#     id education   age  parity  induced  case  spontaneous  stratum  pooled.stratum
+# 0  1.0    0-5yrs  26.0     6.0      1.0   1.0          2.0      1.0             3.0
+# 1  2.0    0-5yrs  42.0     1.0      1.0   1.0          0.0      2.0             1.0
+# 2  3.0    0-5yrs  39.0     6.0      2.0   1.0          0.0      3.0             4.0
+# 3  4.0    0-5yrs  34.0     4.0      2.0   1.0          0.0      4.0             2.0
+# 4  5.0   6-11yrs  35.0     3.0      1.0   1.0          1.0      5.0            32.0
 
 # transform usage
 xf = ToString(columns={'id_1': 'id', 'age_1': 'age'})
@@ -24,9 +24,22 @@ xf = ToString(columns={'id_1': 'id', 'age_1': 'age'})
 # fit and transform
 features = xf.fit_transform(data)
 print(features.head())
-#    age  case    edu_1 education   id  id_1  induced  parity  ...
-# 0  26.0   1.0   0-5yrs    0-5yrs  1.0     0      1.0     6.0 ...
-# 1  42.0   1.0   0-5yrs    0-5yrs  2.0     1      1.0     1.0 ...
-# 2  39.0   1.0   0-5yrs    0-5yrs  3.0     2      2.0     6.0 ...
-# 3  34.0   1.0   0-5yrs    0-5yrs  4.0     3      2.0     4.0 ...
-# 4  35.0   1.0  6-11yrs   6-11yrs  5.0     4      1.0     3.0 ...
+#     id education   age  parity  induced  case  spontaneous  stratum  pooled.stratum      id_1      age_1
+# 0  1.0    0-5yrs  26.0     6.0      1.0   1.0          2.0      1.0             3.0  1.000000  26.000000
+# 1  2.0    0-5yrs  42.0     1.0      1.0   1.0          0.0      2.0             1.0  2.000000  42.000000
+# 2  3.0    0-5yrs  39.0     6.0      2.0   1.0          0.0      3.0             4.0  3.000000  39.000000
+# 3  4.0    0-5yrs  34.0     4.0      2.0   1.0          0.0      4.0             2.0  4.000000  34.000000
+# 4  5.0   6-11yrs  35.0     3.0      1.0   1.0          1.0      5.0            32.0  5.000000  35.000000
+
+print(features.dtypes)
+# id                float32
+# education          object
+# age               float32
+# parity            float32
+# induced           float32
+# case              float32
+# spontaneous       float32
+# stratum           float32
+# pooled.stratum    float32
+# id_1               object  <== string column
+# age_1              object  <== string column
