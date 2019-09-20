ecb5f3b3f1be18a19a1760b120f565f063d03cdd
pieths.dev@gmail.com <PH Schouten>
2019-09-20 15:40:46 -0700

    Add TimeSeriesImputer to the NimbusML project.

--- src/python/nimbusml/timeseries/__init__.py
+++ src/python/nimbusml/timeseries/__init__.py
@@ -3,11 +3,13 @@ from .iidchangepointdetector import IidChangePointDetector
 from .ssaspikedetector import SsaSpikeDetector
 from .ssachangepointdetector import SsaChangePointDetector
 from .ssaforecaster import SsaForecaster
+from .timeseriesimputer import TimeSeriesImputer
 
 __all__ = [
     'IidSpikeDetector',
     'IidChangePointDetector',
     'SsaSpikeDetector',
     'SsaChangePointDetector',
-    'SsaForecaster'
+    'SsaForecaster',
+    'TimeSeriesImputer'
 ]
