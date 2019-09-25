680c593e80962a6981aa8eac4299c848f5ebb2a6
pieths.dev@gmail.com <PH Schouten>
2019-09-25 16:25:47 -0700

    Update schema example.

--- src/python/nimbusml/examples/Schema.py
+++ src/python/nimbusml/examples/Schema.py
@@ -1,28 +1,33 @@
 ###############################################################################
-# Pipeline
+# Get schema from a fitted pipeline example.
 import numpy as np
 import pandas as pd
 from nimbusml import Pipeline, FileDataStream
-from nimbusml.linear_model import FastLinearRegressor
-from nimbusml.preprocessing.normalization import MeanVarianceScaler
-from nimbusml.feature_extraction.text.extractor import Ngram
-from nimbusml import FileDataStream, Pipeline
+from nimbusml.datasets import get_dataset
 from nimbusml.feature_extraction.text import NGramFeaturizer
+from nimbusml.feature_extraction.text.extractor import Ngram
 
-#df = pd.read_csv('E:\\tmp1\\columns.csv', comment='#', header=0, encoding='utf-8')
+# data input (as a FileDataStream)
+path = get_dataset("wiki_detox_train").as_filepath()
 
-X_train = FileDataStream.read_csv("E:\\tmp1\\ag_news.csv")
+data = FileDataStream.read_csv(path, sep='\t')
+print(data.head())
+#    Sentiment                                      SentimentText
+# 0          1  ==RUDE== Dude, you are rude upload that carl p...
+# 1          1  == OK! ==  IM GOING TO VANDALIZE WILD ONES WIK...
+# 2          1  Stop trolling, zapatancas, calling me a liar m...
+# 3          1  ==You're cool==  You seem like a really cool g...
+# 4          1  ::::: Why are you threatening me? I'm not bein...
 
 pipe = Pipeline([
-    NGramFeaturizer(columns={'Column2': 'Column2'}, 
-                    word_feature_extractor = Ngram(ngram_length=3))
+    NGramFeaturizer(
+        word_feature_extractor=Ngram(),
+        columns={
+            'features': ['SentimentText']})
 ])
 
-# max_num_terms = 10 000 000
+pipe.fit(data)
+schema = list(pipe.get_schema())
 
-pipe.fit(X_train)
-print('fit done')
-
-schema = pipe.get_schema()
-print(schema)
-
+print(schema[0:5])
+# ['Sentiment', 'SentimentText', 'features.Char.<␂>|=|=', 'features.Char.=|=|r', 'features.Char.=|r|u']
