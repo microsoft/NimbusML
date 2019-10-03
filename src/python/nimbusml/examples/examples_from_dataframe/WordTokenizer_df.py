414fb4b391fbb900f6eac71c90d5d25975ddb92a
pieths.dev@gmail.com <PH Schouten>
2019-10-03 10:30:09 -0700

    Update the WordTokenizer_df example.

--- src/python/nimbusml/examples/examples_from_dataframe/WordTokenizer_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/WordTokenizer_df.py
@@ -16,7 +16,7 @@ customer_reviews = pandas.DataFrame(data=dict(review=[
     "somewhat dissapointing. I'd probably wont try again",
     "Never visit again... rascals!"]))
 
-tokenize = WordTokenizer(char_array_term_separators=[" ", "n"]) << ['review']
+tokenize = WordTokenizer(char_array_term_separators=[" ", "n"]) << 'review'
 
 pipeline = Pipeline([tokenize])
 
@@ -24,3 +24,11 @@ tokenize.fit(customer_reviews)
 y = tokenize.transform(customer_reviews)
 
 print(y)
+#   review.00 review.01 review.02 review.03 review.04 review.05 review.06 review.07 review.08 review.09 review.10 review.11
+# 0         I    really       did        ot      like       the     taste        of        it      None      None      None
+# 1        It       was  surprisi       gly     quite     good!      None      None      None      None      None      None
+# 2         I      will      ever      ever      ever        go        to      that     place      agai        !!      None
+# 3       The      best    ever!!        It       was     amazi       gly      good         a         d     super      fast
+# 4         I      wish         I       had        go         e  earlier,        it       was      that     great      None
+# 5  somewhat  dissapoi        ti        g.       I'd  probably        wo         t       try      agai      None      None
+# 6     Never     visit      agai       ...  rascals!      None      None      None      None      None      None      None
