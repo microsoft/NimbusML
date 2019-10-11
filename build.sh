6e0b016bb0fe7cb191bb11698689e31a4d14d1f3
Mustafa Bal <mubal@microsoft.com>
2019-10-11 13:43:57 -0700

    Revert "Temporary change to build.sh to view contents of /lib/x86_64-linux-gnu""
    This reverts commit 6f43fef61b24a1e06ba8fe539c64ae07aa3e1754.

--- build.sh
+++ build.sh
@@ -206,10 +206,6 @@ then
     cp  "${BuildOutputDir}/${__configuration}"/DotNetBridge.dll "${__currentScriptDir}/src/python/nimbusml/internal/libs/"
     cp  "${BuildOutputDir}/${__configuration}"/pybridge.so "${__currentScriptDir}/src/python/nimbusml/internal/libs/"
 
-    echo "start"
-    ls "/lib/x86_64-linux-gnu/"
-    echo "finish"
-
 	# ls "${BuildOutputDir}/${__configuration}/Platform/${PublishDir}"/publish/
     if [ ${PythonVersion} = 2.7 ]
     then
