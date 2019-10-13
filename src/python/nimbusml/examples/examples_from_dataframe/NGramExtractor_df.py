8ac85c6ed426577ea64f0bea8d60b195f84dfcec
Mustafa Bal <mubal@microsoft.com>
2019-10-12 17:50:05 -0700

    Update NGramExtractor_df.py

--- src/python/nimbusml/examples/examples_from_dataframe/NGramExtractor_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/NGramExtractor_df.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 ###############################################################################
 # Example with NGramExtractor and LogisticRegressionBinaryClassifier
 import pandas
