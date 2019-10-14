c08890394e8dfb6582c3590ccfbc14a15ed87675
Mustafa Bal <mubal@microsoft.com>
2019-10-14 10:05:23 -0700

    To re-run build tests

--- src/python/nimbusml/examples/examples_from_dataframe/NGramExtractor_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/NGramExtractor_df.py
@@ -1,5 +1,4 @@
 # -- coding: utf-8 --
-
 ###############################################################################
 # Example with NGramExtractor and LogisticRegressionBinaryClassifier
 import pandas
