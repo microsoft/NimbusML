b3c6d6211f7ae2b8f773186296d973341a6957cb
pieths.dev@gmail.com <PH Schouten>
2019-10-30 16:10:39 -0700

    Initial implementation of the OnnxRunner transform.

--- src/python/nimbusml/preprocessing/__init__.py
+++ src/python/nimbusml/preprocessing/__init__.py
@@ -2,10 +2,12 @@ from .fromkey import FromKey
 from .tokey import ToKey
 from .tensorflowscorer import TensorFlowScorer
 from .datasettransformer import DatasetTransformer
+from .onnxrunner import OnnxRunner
 
 __all__ = [
     'FromKey',
     'ToKey',
     'TensorFlowScorer',
-    'DatasetTransformer'
+    'DatasetTransformer',
+    'OnnxRunner'
 ]
