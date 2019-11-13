9ae46fc6e7c26b2d43e40c8f45f0a08e5fc4c7c7
pieths.dev@gmail.com <PH Schouten>
2019-11-13 13:16:11 -0800

    Modifications to support scripted temp/docs merging.

--- src/python/nimbusml/examples/examples_from_dataframe/WordEmbedding_df.py
+++ src/python/nimbusml/examples/examples_from_dataframe/WordEmbedding_df.py
@@ -2,8 +2,7 @@
 # WordEmbedding: pre-trained transform to generate word embeddings
 import pandas
 from nimbusml import Pipeline
-from nimbusml.feature_extraction.text import WordEmbedding
-from nimbusml.feature_extraction.text.ngramfeaturizer import NGramFeaturizer
+from nimbusml.feature_extraction.text import NGramFeaturizer, WordEmbedding
 from nimbusml.feature_extraction.text.extractor import Ngram
 
 # create the data
@@ -22,5 +21,12 @@ pipeline = Pipeline([
 ])
 y = pipeline.fit_transform(customer_reviews)
 
-# view the review embeddings
-# print(y.head())
+# view a small subset of the review embeddings
+print(y.iloc[:5, -3:])
+#    review_TransformedText.147  review_TransformedText.148  review_TransformedText.149
+# 0                    1.918661                   -0.714531                    3.062141
+# 1                    1.891922                   -0.248650                    1.706620
+# 2                    1.601611                    0.309785                    3.379576
+# 3                    1.970666                    1.477450                    3.110802
+# 4                    2.521791                    0.122538                    3.129919
+
